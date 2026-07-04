e384e52b5e0bd3387e701952b8f4ab1fe3460bde
fareed1813 <fareed1813@users.noreply.github.com>
2026-07-04 16:21:20 +0530

    build: split vendors to resolve chunk size limit warning

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,5 +5,23 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173
+  },
+  build: {
+    chunkSizeWarningLimit: 800,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('lucide-react')) {
+              return 'vendor_lucide';
+            }
+            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
+              return 'vendor_react';
+            }
+            return 'vendor';
+          }
+        }
+      }
+    }
   }
 });
